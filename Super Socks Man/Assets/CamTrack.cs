e126324fb48c7747f25e23b84cfd338f41524c5c
Eleanor <eap9238@g.rit.edu>
2019-01-16 15:11:50 -0500

    Made the camera mode that prevents backtracking, and set up a basic POC course to run on. still only using standard assets, script still requires manual target assignment for now, as this is only POC.

---
+++ Super Socks Man/Assets/CamTrack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTrack : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target.transform.position.x >= transform.position.x + 2)
+        {
+            //transform.position += new Vector3(target.transform.position.x - transform.position.x, 0, 0) * 3 * Time.deltaTime;
+
+            transform.position = new Vector3(target.transform.position.x - 2, 0, -10);
+        }
+	}
+}
