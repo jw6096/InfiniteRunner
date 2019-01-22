04d3f53cd3212578bcd0f9de916d9b6be0c079a6
DALEK-PC\Eleanor <eap9238@g.rit.edu>
2019-01-21 19:46:35 -0500

    Added the distance counter, set the boundaries to turn the object red and send a trigger message to simulate 'death'.

---
+++ Super Socks Man/.vs/Assets/CamTrack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamTrack : MonoBehaviour {
+
+	public float baseSpeed;
+	public float maxSpeed;
+	public float incRate;
+
+	public Text distCount;
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = baseSpeed;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (speed != maxSpeed)
+		{
+			if (speed < maxSpeed)
+			{
+				speed += incRate;
+			}
+			else
+			{
+				speed = maxSpeed;
+			}
+		}
+
+		transform.position += new Vector3(speed, 0, -10) * Time.deltaTime;
+
+		distCount.text = "Distance: " + transform.position.x.ToString("F2");
+	}
+}
---
+++ Super Socks Man/.vs/Assets/CameraDeathTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDeathTrigger : MonoBehaviour
+{
+	public BoxCollider2D leftCollider;
+	public BoxCollider2D rightCollider;
+	public BoxCollider2D topCollider;
+	public BoxCollider2D bottomCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		leftCollider.offset = new Vector2 ((-1) * Screen.width / 100, 0);
+		leftCollider.size = new Vector2 (1, Screen.height / 50);
+
+		rightCollider.offset = new Vector2 (Screen.width / 100, 0);
+		rightCollider.size = new Vector2 (1, Screen.height / 50);
+
+		topCollider.offset = new Vector2 (0, (-1) * Screen.height / 100);
+		topCollider.size = new Vector2 (Screen.width / 50, 1);
+
+		bottomCollider.offset = new Vector2 (0, Screen.height / 100);
+		bottomCollider.size = new Vector2 (Screen.width / 50, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Debug.Log ("Collision: ");
+		//Debug.Log (other);
+		//Debug.Log (other.gameObject.GetComponent<SpriteRenderer> ().color);
+
+		//other.gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+
+		Debug.Log ("Death Collision");
+		other.gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+	}
+}
