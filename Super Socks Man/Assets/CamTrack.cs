add3dd124d4b506728169d2ff762b35860fdf39a
Eleanor Post (RIT Student) <eap9238@ad.rit.edu>
2019-01-23 10:58:41 -0500

    perfect goddamn screen framing

--- Super Socks Man/Assets/CamTrack.cs
+++ Super Socks Man/Assets/CamTrack.cs
@@ -9,11 +9,29 @@ public class CamTrack : MonoBehaviour {
     public float incRate;
 
     private float speed;
+    private BoxCollider2D[] targetZone;
+    private Camera camera;
 
 	// Use this for initialization
 	void Start () {
         speed = baseSpeed;
-	}
+
+        camera = gameObject.GetComponent<Camera>();
+
+        targetZone = gameObject.GetComponents<BoxCollider2D>();
+
+        targetZone[0].offset = new Vector2(-Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x + 0.5f, 0);
+        targetZone[0].size = new Vector2(1, Camera.main.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, 0)).y * 2);
+
+        targetZone[1].offset = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x - 0.5f, 0);
+        targetZone[1].size = new Vector2(1, Camera.main.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, 0)).y * 2);
+
+        targetZone[2].offset = new Vector2(0, -Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y + 0.5f);
+        targetZone[2].size = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, 0)).x * 2, 1);
+
+        targetZone[3].offset = new Vector2(0, Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y - 0.5f);
+        targetZone[3].size = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, 0)).x * 2, 1);
+    }
 	
 	// Update is called once per frame
 	void Update () {
