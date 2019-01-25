36fc8a0314bf0d0d44b387b2825b2daeeaecf3a7
Matthew Barry <mxb9517@rit.edu>
2019-01-24 21:18:57 -0500

    - Added in platform generation - Added in background generation - Added in the new player with sprite sheet - Added in all new models and audio - Set up all the scenes for tomorrow's place test - Created UI functionality with death screen - Organized project folder structure

---
+++ Super Socks Man/Assets/Scripts/CamTrack.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CamTrack : MonoBehaviour {
+
+    public float baseSpeed;
+    public float maxSpeed;
+	public float incRate;	
+
+	public Text distCount;
+    public GameObject deathScreen;
+    public bool gameOver = false;
+    private float distanceSinceReset = 0;
+    private float distanceAtLastReset = 0;
+
+    private float speed;
+    private BoxCollider2D[] targetZone;
+    private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        speed = baseSpeed;
+
+        camera = gameObject.GetComponent<Camera>();
+
+        targetZone = gameObject.GetComponents<BoxCollider2D>();
+
+        targetZone[0].offset = new Vector2(-Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x + 0.5f, 0);
+        targetZone[0].size = new Vector2(1, Camera.main.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, 0)).y * 2);
+
+        targetZone[1].offset = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x - 0.5f, 0);
+        targetZone[1].size = new Vector2(1, Camera.main.ScreenToWorldPoint(new Vector3(0, camera.pixelHeight, 0)).y * 2);
+
+        targetZone[2].offset = new Vector2(0, -Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y + 0.5f);
+        targetZone[2].size = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, 0)).x * 2, 1);
+
+        targetZone[3].offset = new Vector2(0, Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y - 0.5f);
+        targetZone[3].size = new Vector2(Camera.main.ScreenToWorldPoint(new Vector3(camera.pixelWidth, 0, 0)).x * 2, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!gameOver)
+        {
+            if (speed != maxSpeed)
+            {
+                if (speed < maxSpeed)
+                {
+                    speed += incRate;
+                }
+                else
+                {
+                    speed = maxSpeed;
+                }
+            }
+        }
+
+		transform.position += new Vector3(speed, 0, -10) * Time.deltaTime;
+
+		distCount.text = "Distance: " + transform.position.x.ToString("F2");
+        distanceSinceReset = transform.position.x - distanceAtLastReset;
+
+        if(distanceSinceReset >= 5)
+        {
+            GetComponent<PlatformGeneration>().GenerateBackground();
+            GetComponent<PlatformGeneration>().GeneratePlatforms();
+            distanceSinceReset = 0;
+            distanceAtLastReset = transform.position.x;
+        }
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Debug.Log ("Collision: ");
+		//Debug.Log (other);
+		//Debug.Log (other.gameObject.GetComponent<SpriteRenderer> ().color);
+
+		//other.gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+
+		Debug.Log ("Death Collision");
+		other.gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+        deathScreen.SetActive(true);
+        speed = 0;
+        gameOver = true;
+	}
+}
---
+++ Super Socks Man/Assets/Scripts/PlatformGeneration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGeneration : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject[] platforms;
+    private int backgroundsGenerated = 1;
+    private int platformsGenerated = 2;
+
+    public void GenerateBackground()
+    {
+        Instantiate(background, new Vector3((16 * backgroundsGenerated) + 11, 0, 1), Quaternion.identity);
+        backgroundsGenerated++;
+    }
+
+    public void GeneratePlatforms()
+    {
+        float randomY = Random.Range(-2.0f, 2.0f);
+        Debug.Log(randomY);
+        int platformNum = Random.Range(0, 2);
+        Instantiate(platforms[platformNum], new Vector3(11 * platformsGenerated, randomY, 0), Quaternion.identity);
+        platformsGenerated++;
+    }
+}
