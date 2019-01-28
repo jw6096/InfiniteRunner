62ceb4895b9c616880553088d4444cda1153f572
LoganL <ll8875@g.rit.edu>
2019-01-28 10:52:27 -0500

    More main menu UI and start of game manager

--- Super Socks Man/Assets/Scripts/MenuScripts.cs
+++ Super Socks Man/Assets/Scripts/MenuScripts.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MenuScripts : MonoBehaviour
 {
+    int menuNum;
+    public GameObject[] menus;
+    public GameManager gMan;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        menuNum = 0;
 	}
 	
 	// Update is called once per frame
@@ -18,8 +21,31 @@ public class MenuScripts : MonoBehaviour
 		
 	}
 
+    /// <summary>
+    /// Switches the current scene to the chosen one, by string
+    /// </summary>
+    /// <param name="sceneName"></param>
     public void SwitchScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Switches the currently active menu to the newly choesen menu
+    /// </summary>
+    /// <param name="menuPosition"></param>
+    public void SwitchMenu(int menuPosition)
+    {
+        if(menuPosition >= 0 && menuPosition < menus.Length)
+        {
+            menus[menuNum].SetActive(false);
+            menus[menuPosition].SetActive(true);
+            menuNum = menuPosition;
+        }
+    }
+
+    public void PurchaseItem(int cost)
+    {
+        
     }
 }
