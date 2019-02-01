8a29e2a270d391c832ad885864c8bd36d304e557
ll8875 <ll8875@rit.edu>
2019-02-01 03:17:21 -0500

    -Updated the menus to have a basic functioning store -Fixed the main menus aspect ratio -Basic Game Manager that interacts with the menu and saves some settings between scenes

--- Super Socks Man/Assets/Scripts/GameManager.cs
+++ Super Socks Man/Assets/Scripts/GameManager.cs
@@ -4,29 +4,82 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+    //public Camera mainCamera;
+    AudioSource musicSource;
 
-    int coins;
+    //Store item array
+    GameObject[] storeItems;
+    bool[] purchasedItems;
 
-    int Coins
+    static int coins = 200;
+    float musicVolume = 1.0f;
+
+    public GameObject[] StoreItems
+    {
+        get { return storeItems; }
+        set { storeItems = value; }
+    }
+
+    public bool[] PurchasedItems
+    {
+        get { return purchasedItems; }
+        set { purchasedItems = value; }
+    }
+
+    public int Coins
     {
         get { return coins; }
+        set
+        {
+            if(coins <= 0)
+            {
+                coins = 0;
+            }
+            else
+            {
+                coins = value;
+            }
+        }
     }
 
     private void Awake()
     {
+        if(GameManager.instance == null)
+        {
+            GameManager.instance = this;
+        }
+        else if(GameManager.instance != this)
+        {
+            Destroy(gameObject);
+        }
+
         DontDestroyOnLoad(this);
+
     }
 
     // Use this for initialization
     void Start ()
     {
-        //Test purposes
-        coins = 100000;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        //if(mainCamera == null)
+        //{
+        //    mainCamera = Camera.main;
+        //}
+        //musicSource = mainCamera.GetComponent<AudioSource>();
+        musicSource = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+        musicSource.volume = musicVolume;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 		
 	}
+
+    //Options Methods
+    public void ChangeMusicVolume(float volume)
+    {
+        musicVolume = volume;
+        musicSource.volume = volume;
+    }
 }
--- Super Socks Man/Assets/Scripts/MenuScripts.cs
+++ Super Socks Man/Assets/Scripts/MenuScripts.cs
@@ -1,25 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuScripts : MonoBehaviour
 {
     int menuNum;
+    int currStoreSlot;
     public GameObject[] menus;
+    public GameObject storePanel;
+    GameObject selectedStoreItem;
+    Toggle storeItemToggle;
     public GameManager gMan;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         menuNum = 0;
-	}
+        gMan = GameManager.instance;
+
+        if(storePanel != null)
+        {
+            int itemCount = storePanel.transform.childCount;
+
+            GameObject[] storeItems = new GameObject[itemCount];
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                storeItems[i] = storePanel.transform.GetChild(i).gameObject;
+            }
+
+            //Re-Create the list of store items
+            gMan.StoreItems = storeItems;
+
+            //Check if a list of purchased items needs to be created
+            if(gMan.PurchasedItems == null)
+            {
+                gMan.PurchasedItems = new bool[itemCount];
+            }
+            //Otherwise update the shop
+            else
+            {
+                for(int i = 0; i < itemCount; i++)
+                {
+                    storeItems[i].GetComponent<Toggle>().interactable = !gMan.PurchasedItems[i];
+                }
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+
+    }
 
     /// <summary>
     /// Switches the current scene to the chosen one, by string
@@ -44,8 +79,70 @@ public class MenuScripts : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Changes the selected store item
+    /// </summary>
+    /// <param name="arrayPos"></param>
+    public void SelectStoreItem(int arrayPos)
+    {
+        GameObject item = gMan.StoreItems[arrayPos];
+        currStoreSlot = arrayPos;
+        //First Item
+        if (selectedStoreItem == null)
+        {
+            selectedStoreItem = item;
+            storeItemToggle = item.GetComponent<Toggle>();
+            selectedStoreItem.GetComponent<Image>().color = new Color(0, 0, 255);
+        }
+        //Same Item
+        else if (selectedStoreItem == item)
+        {
+            if (storeItemToggle.isOn)
+            {
+                selectedStoreItem.GetComponent<Image>().color = new Color(0, 0, 255);
+            }
+            else
+            {
+                selectedStoreItem.GetComponent<Image>().color = new Color(255, 255, 255);
+            }
+        }
+        //New Item
+        else
+        {
+            if (storeItemToggle.isOn)
+            {
+                selectedStoreItem.GetComponent<Image>().color = new Color(255, 255, 255);
+            }
+
+            selectedStoreItem = item;
+            storeItemToggle = item.GetComponent<Toggle>();
+            selectedStoreItem.GetComponent<Image>().color = new Color(0, 0, 255);
+        }
+    }
+
+    /// <summary>
+    /// Purchases the item and disables the current item
+    /// </summary>
+    /// <param name="cost"></param>
     public void PurchaseItem(int cost)
     {
-        
+        if(storeItemToggle == null || storeItemToggle.isOn == false)
+        {
+            Debug.Log("No item selected");
+            return;
+        }
+        if (gMan.Coins >= cost)
+        {
+            gMan.Coins -= cost;
+            storeItemToggle.isOn = false;
+
+            storeItemToggle.interactable = false;
+            gMan.PurchasedItems[currStoreSlot] = true;
+        }
+        else
+        {
+            Debug.Log("Not enough coins to purchase");
+        }
+
     }
 }
