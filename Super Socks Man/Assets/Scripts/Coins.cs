d2795552ee04bfbd3edd3cb9191fb80f6f49b61c
jxw60961 <joeywangyang@gmail.com>
2019-02-01 06:08:17 +0000

    added code to prevent music from restarting everytime and added coin system

---
+++ Super Socks Man/Assets/Scripts/Coins.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour
+{
+    private CamTrack camTrack;
+    private float coins;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTrack = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CamTrack>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (camTrack.deathScreen && !camTrack.check)
+        {
+            coins += (float)Math.Round(camTrack.distTraveled);
+            text.text = "Total Coins: " + coins.ToString();
+            camTrack.check = true;
+        }
+    }
+}
